fad655bcb017d83e1669a72f1fe736105f96606c
Juan Vargas <juandeveloper19@gmail.com>
2021-09-12 04:05:16 -0500

    Initial configuration of the structure and general styles of the project

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Home = () => {
+	return (
+		<div className="home-page">
+			<h1 className="title-styles">
+				Pagina de <span>inicio</span>
+			</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis tempora, consectetur rem eveniet
+				asperiores, commodi ut odio architecto sint iure dolore nulla ullam quia natus alias facilis vel,
+				quisquam dolores?
+			</p>
+		</div>
+	);
+};
+
+export default Home;
